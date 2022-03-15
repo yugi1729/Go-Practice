7885d1f6ad715606742fc469311c7e3b73b2a94e
Yugandhar Desai <yugandhar.d.desai@gmail.com>
2022-03-16 01:35:46 +0530

    added few practice scripts

---
+++ Factorial.go
@@ -0,0 +1,27 @@
+package main 
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func memoizedFact(n int) int {
+	memo := make(map[int]int)
+	if n == 0 || n == 1 {
+		return 1
+	}
+	if val := memo[n]; val != 0 {
+		return val
+	}
+	memo[n] = n * memoizedFact(n-1)
+	return memo[n]
+}
+
+func main() {
+	fmt.Println(fact(5))
+	fmt.Println(memoizedFact(10))
+}
